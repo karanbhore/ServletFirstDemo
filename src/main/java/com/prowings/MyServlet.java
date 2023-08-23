3867cc88270ab0c39b10220d8282952bca601845
karan <karanbhore64@gmail.com>
2023-08-23 19:58:42 +0530

    .

---
+++ src/main/java/com/prowings/MyServlet.java
@@ -0,0 +1,21 @@
+package com.prowings;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.println("I am in do get method for browser");
+
+		System.out.println("i am in do get method");
+	}
+
+}
